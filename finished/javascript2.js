7c34124655b7d62113886a1205a58589d1505a8e
bochra98 <b.larbi@esi-sba.dz>
2019-07-11 21:13:49 +0200

    Add files via upload

---
+++ finished/javascript2.js
@@ -0,0 +1,26 @@
+
+var counter=0;
+function count()
+{
+  $('.show').addClass("notification");
+            $(".show").html(counter);
+}
+ $('.add').on('click',function(){
+      counter++
+      count();
+ });
+    
+
+/********************************************for the message after adding a product to cart*******************/
+function launch_toast() {
+    var x = document.getElementById("toast")
+    x.className = "appear";
+    setTimeout(function(){ x.className = x.className.replace("appear", ""); }, 5000);
+}
+/******************************************************************************************************************/
+function addAndBuy(){
+	launch_toast();
+	
+}
+
+
